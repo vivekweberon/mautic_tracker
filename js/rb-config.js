37f9695d635e05aba4bf819479df67de34e6c269
vivekweberon <vivek@weberon.net>
2025-06-05 11:58:42 +0530

    Enabling rollbar config

--- js/rb-config.js
+++ js/rb-config.js
@@ -14,24 +14,24 @@ const ROLLBAR_CONFIG = {
   "realtor.reachpersona.com" : "Production-reachpersona"
 }
 
-// var _rollbarConfig = {
-//   accessToken: 'a369216935fe434cb70adca043caf58dae1824f3f9c85ebab328f55fba0d366c13fb124a5c15f323a0f1f390bcb24c5b',
-//   captureUncaught: true,
-//   captureUnhandledRejections: true,
-//   retryInterval: 5000,
-//   autoInstrument: {
-//     network: true,
-//     log: true,
-//     dom: true,
-//     navigation: true,
-//     connectivity: true,
-//     contentSecurityPolicy: true,
-//     errorOnContentSecurityPolicy: true
-//   }, 
-//   payload: {
-//       environment: ''
-//   }
-// };
+var _rollbarConfig = {
+  accessToken: 'a369216935fe434cb70adca043caf58dae1824f3f9c85ebab328f55fba0d366c13fb124a5c15f323a0f1f390bcb24c5b',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+  retryInterval: 5000,
+  autoInstrument: {
+    network: true,
+    log: true,
+    dom: true,
+    navigation: true,
+    connectivity: true,
+    contentSecurityPolicy: true,
+    errorOnContentSecurityPolicy: true
+  }, 
+  payload: {
+      environment: ''
+  }
+};
 
 updateRollbarEnvironment();
 updateRollbarPerson();
