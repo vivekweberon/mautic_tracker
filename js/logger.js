cc01079f03dc549ce0f132bd7ce010ce476753eb
vivekweberon <vivek@weberon.net>
2025-06-20 16:45:51 +0530

    Testing Rollbar

--- js/logger.js
+++ js/logger.js
@@ -18,39 +18,39 @@ if(_rollbarConfig){
 //   return false;
 // }
 
-function logResourceLoadError(input) {
-    let src = "unknown";
+// function logResourceLoadError(input) {
+//     let src = "unknown";
 
-    // If called with an event object (e.g., from onError handler)
-    if (input?.target || input?.currentTarget || input?.srcElement) {
-      console.log("logResourceLoadError called with event - server", input);
-      src = input.currentTarget?.src || input.target?.src || input.srcElement?.src || "unknown";
-    } 
-    // If called with a direct ref (like an img or link element)
-    else if (input?.src || input?.href) {
-      console.log("logResourceLoadError called with ref - client", input);
-      src = input.src || input.href || "unknown";
-    } 
-    // Fallback
-    else {
-      console.log("logResourceLoadError called with unknown input", input);
-    }
+//     // If called with an event object (e.g., from onError handler)
+//     if (input?.target || input?.currentTarget || input?.srcElement) {
+//       console.log("logResourceLoadError called with event - server", input);
+//       src = input.currentTarget?.src || input.target?.src || input.srcElement?.src || "unknown";
+//     } 
+//     // If called with a direct ref (like an img or link element)
+//     else if (input?.src || input?.href) {
+//       console.log("logResourceLoadError called with ref - client", input);
+//       src = input.src || input.href || "unknown";
+//     } 
+//     // Fallback
+//     else {
+//       console.log("logResourceLoadError called with unknown input", input);
+//     }
 
-    const err = `Error loading: '${src}'`;
-    if (typeof Rollbar !== "undefined" && Rollbar.error) {
-      Rollbar.error(err);
-    }
-    console.log(err);
-    return false;
-  }
+//     const err = `Error loading: '${src}'`;
+//     if (typeof Rollbar !== "undefined" && Rollbar.error) {
+//       Rollbar.error(err);
+//     }
+//     console.log(err);
+//     return false;
+//   }
 
-function logError(err){
-  if(window.Rollbar){
-    Rollbar.error(err);
-  }else{
-    console.log(err);
-  }
-}
+// function logError(err){
+//   if(window.Rollbar){
+//     Rollbar.error(err);
+//   }else{
+//     console.log(err);
+//   }
+// }
 
 function getClientInfo(){
   let client = {};
